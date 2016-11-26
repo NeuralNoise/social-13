e5bfa1edf75f2cd70201efc22d5f14a238f725cf
pochta2id <pochta2id@gmail.com>
2016-11-26 05:21:10 +0300

    Social.php now can move boxes!!!

--- templates/intranet/main.php
+++ templates/intranet/main.php
@@ -1,11 +1,7 @@
 <div class="post">
 	<img class="post_user_logo" src="/images/ilya_logo.jpg" />
-	<div class="post_form" contenteditable="true">
-		Test message
-	</div>
-	<div id="post_photos_preview" class="post_photos_preview">
-		
-	</div>
+	<div class="post_form" contenteditable="true">Test message</div>
+	<div id="post_photos_preview" class="post_photos_preview"></div>
 	<div class="post_foot">
 		<div class="post_elements">
 			<div class="photik">
@@ -22,7 +18,8 @@
 
 
 <script>
-  var post_photo_thumbnail=0
+  var post_photo_thumbnail=1
+  var max_photo_thumbnail=8
   function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
 
@@ -34,39 +31,56 @@
         continue;
       }
 
-      var reader = new FileReader();
+      var reader = new FileReader(); 
 
       // Closure to capture the file information.
-      reader.onload = (function(theFile){
-        return function(e) {
-			if(window.post_photo_thumbnail==0){
-				//Create clear box
-				div_clear = document.createElement('div');
-				div_clear.id="div_photo_clear";
-				div_clear.className="clear";
-				document.getElementById('post_photos_preview').insertBefore(div_clear, null);
-
-				//Create plusik box
-				div_plusik = document.createElement('div');
-				div_plusik.id="post_preview_plusik";
-				div_plusik.className="post_preview_plusik";
-				div_plusik.innerHTML= '<input type="file" id="filesnew" name="files[]" class="photik photik_big" value="" multiple />'
-				document.getElementById('post_photos_preview').insertBefore(div_plusik, div_clear);
-				document.getElementById('filesnew').addEventListener('change', handleFileSelect, false);
-			}
-			
-			var div = document.createElement('div');
-			div.className='post_photo_preview';
-			div.setAttribute('draggable', true);
-			div.innerHTML = ['<img class="post_photo_preview" src="', e.target.result,
-							'" title="', escape(theFile.name), '"/>'].join('');
-			document.getElementById('post_photos_preview').style="border-top:1px solid #CCC;";
-			document.getElementById('post_photos_preview').insertBefore(div, document.getElementById('post_preview_plusik'));
-			
-			//Increase number of thumbnail
-			window.post_photo_thumbnail=window.post_photo_thumbnail+1
-        };
-      })(f);
+      reader.onload = (function(theFile){			
+			return function(e) {
+				if(window.post_photo_thumbnail==1){
+					//Create clear box
+					div_clear = document.createElement('div');
+					div_clear.id="div_photo_clear";
+					div_clear.className="clear";
+					
+					//Insert HTML
+					document.getElementById('post_photos_preview').insertBefore(div_clear, null);
+					
+					//Create plusik box
+					div_plusik = document.createElement('div');
+					div_plusik.id="post_preview_plusik";
+					div_plusik.className="post_preview_plusik";
+					div_plusik.innerHTML= '<input type="file" id="filesnew" name="files[]" class="photik photik_big" value="" multiple />'
+					document.getElementById('post_photos_preview').insertBefore(div_plusik, div_clear);
+					document.getElementById('filesnew').addEventListener('change', handleFileSelect, false);			
+				}
+				if(window.post_photo_thumbnail<=window.max_photo_thumbnail){
+					var div = document.createElement('div');
+					div.className='post_photo_preview';
+					div.setAttribute('draggable', true);
+					div.innerHTML = ['<img class="post_photo_preview" src="', e.target.result,
+									'" title="', escape(theFile.name), '"/>'].join('');
+					document.getElementById('post_photos_preview').style="border-top:1px solid #CCC;";
+					
+					//Define before which element I should add a child
+					if(window.post_photo_thumbnail<window.max_photo_thumbnail){
+						add_before_id='post_preview_plusik'
+					}else{
+						add_before_id='div_photo_clear'
+						document.getElementById('post_photos_preview').removeChild(document.getElementById('post_preview_plusik'))						
+						
+					}
+					
+					document.getElementById('post_photos_preview').insertBefore(div, document.getElementById(add_before_id));
+					
+					//Increase number of thumbnail
+					window.post_photo_thumbnail=window.post_photo_thumbnail+1 
+					//Init drag boxes
+					initDrag(div);
+				}
+	
+								
+			};
+	  })(f);
 
       // Read in the image file as a data URL.
       reader.readAsDataURL(f);
@@ -76,19 +90,28 @@
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
   
   
-  
+//---------- 
 //Drag boxes
-
-
-
-function handleDragStart(e) {
-  this.style.opacity = '0.4';  // this / e.target is the source node.
+//----------  
+var dragSrcEl = null;
+function initDrag(col){
+	col.addEventListener('dragstart', handleDragStart, false);
+	col.addEventListener('dragenter', handleDragEnter, false)
+	col.addEventListener('dragover', handleDragOver, false);
+	col.addEventListener('dragleave', handleDragLeave, false);
+	col.addEventListener('drop', handleDrop, false);
+	col.addEventListener('dragend', handleDragEnd, false);
 }
 
-var cols = document.querySelectorAll('#post_photos_preview .post_photo_preview');
-[].forEach.call(cols, function(col) {
-  col.addEventListener('dragstart', handleDragStart, false);
-});
+function handleDragStart(e) {
+  // Target (this) element is the source node.
+  //this.style.opacity = '0.4';
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
 
 //Punktir border over{
 function handleDragOver(e) {
@@ -109,61 +132,7 @@ function handleDragEnter(e) {
 function handleDragLeave(e) {
   this.classList.remove('over');  // this / e.target is previous target element.
 }
-
-var cols = document.querySelectorAll('#columns .post_photo_preview');
-[].forEach.call(cols, function(col) {
-  col.addEventListener('dragstart', handleDragStart, false);
-  col.addEventListener('dragenter', handleDragEnter, false);
-  col.addEventListener('dragover', handleDragOver, false);
-  col.addEventListener('dragleave', handleDragLeave, false);
-});
-
 //}
-
-
-function handleDrop(e) {
-  // this / e.target is current target element.
-
-  if (e.stopPropagation) {
-    e.stopPropagation(); // stops the browser from redirecting.
-  }
-
-  // See the section on the DataTransfer object.
-
-  return false;
-}
-
-function handleDragEnd(e) {
-  // this/e.target is the source node.
-
-  [].forEach.call(cols, function (col) {
-    col.classList.remove('over');
-  });
-}
-
-var cols = document.querySelectorAll('#columns .post_photo_preview');
-[].forEach.call(cols, function(col) {
-  col.addEventListener('dragstart', handleDragStart, false);
-  col.addEventListener('dragenter', handleDragEnter, false)
-  col.addEventListener('dragover', handleDragOver, false);
-  col.addEventListener('dragleave', handleDragLeave, false);
-  col.addEventListener('drop', handleDrop, false);
-  col.addEventListener('dragend', handleDragEnd, false);
-});
-
-
-//Drag transfer{
-var dragSrcEl = null;
-
-function handleDragStart(e) {
-  // Target (this) element is the source node.
-  this.style.opacity = '0.4';
-
-  dragSrcEl = this;
-
-  e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html', this.innerHTML); 
-}
 
 
 function handleDrop(e) {
@@ -182,7 +151,12 @@ function handleDrop(e) {
 
   return false;
 }	
-//}
 
- 
+function handleDragEnd(e){
+  // this/e.target is the source node.
+
+  [].forEach.call(cols, function (col) {
+    col.classList.remove('over');
+  });
+}
 </script>
